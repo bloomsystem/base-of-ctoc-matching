a86b2ed6bd08ba73a12f1564eb59823617fadb82
h.nagano <h-nagano@bloom-system.tech>
2021-12-02 00:03:37 +0900

    Fix too many connection in dev

--- source/freelance/next-env.d.ts
+++ source/freelance/next-env.d.ts
@@ -1,3 +1,11 @@
 /// <reference types="next" />
 /// <reference types="next/types/global" />
 /// <reference types="next/image-types/global" />
+
+declare global {
+    namespace NodeJS {
+        interface Global {
+            prisma: any;
+        }
+    }
+}
--- source/freelance/pages/api/client.ts
+++ source/freelance/pages/api/client.ts
@@ -1,9 +1,24 @@
 import { PrismaClient } from '@prisma/client'
 
-const prisma = new PrismaClient(
-    {
-        log: ["query", "error", "info", "warn"]
+// Tips: https://qiita.com/Gma_Gama/items/5b637f746b8df62fcc62
+
+declare global {
+    var prisma: PrismaClient;
+}
+
+let prisma: PrismaClient;
+
+if (process.env.NODE_ENV === "production") {
+    prisma = new PrismaClient({
+        log: ["error"]
+    })
+} else {
+    if (!global.prisma) {
+        global.prisma = new PrismaClient({
+            log: ["query", "error", "info", "warn"]
+        })
     }
-)
+    prisma = global.prisma
+}
 
 export default prisma
